e2999bd14ad40612ab0ca92e7b78e5b5f8a88d40
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-03-23 04:59:10 +0600

    Price updated in UI

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({cart}) => {
+    let totalPrice = 0
+    for(const product of cart){
+        totalPrice += product.price
+    }
+
     return (
-        <div>
-            <h3>Order summary</h3>
-            <p>Selected products: {cart.length}</p>
+        <div className="cart">
+            <div className="cart-text">
+                <h3>Order summary</h3>
+                <p>Selected products: {cart.length}</p>
+                <p>Total Price: {totalPrice}</p>
+                <p>Total Shipping Charge: </p>
+                <p>Tax: </p>
+                <h4>Grand total: </h4>
+            </div>
         </div>
     );
 };
