ff75408cab86fb9487ad902e8fdd6a2623cf6ece
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-03-23 17:14:24 +0600

    all value updated in UI and set data in local storage

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,20 +2,25 @@ import React from 'react';
 import './Cart.css'
 
 const Cart = ({cart}) => {
+    console.log(cart)
     let totalPrice = 0
+    let shippingCharge = 0
     for(const product of cart){
         totalPrice += product.price
+        shippingCharge += product.shipping
     }
+    const tex = totalPrice * 0.1
+    const grandTotalPrice = totalPrice + shippingCharge + tex
 
     return (
         <div className="cart">
             <div className="cart-text">
                 <h3>Order summary</h3>
                 <p>Selected products: {cart.length}</p>
-                <p>Total Price: {totalPrice}</p>
-                <p>Total Shipping Charge: </p>
-                <p>Tax: </p>
-                <h4>Grand total: </h4>
+                <p>Total Price: ${totalPrice}</p>
+                <p>Total Shipping Charge: ${shippingCharge}</p>
+                <p>Tax: ${tex.toFixed(2)}</p>
+                <h4>Grand total: ${grandTotalPrice}</h4>
             </div>
         </div>
     );
